2b652cba1d7522ef05940e39d956d5de102ef1d5
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-18 18:51:53 -0500

    add hooks and secure password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
 
     User.update(req.body, {
+        individualHooks: true,
         where: {
           id: req.params.id
         }
